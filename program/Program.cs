23790ade92fdfbed1b4655514ad6fa27b1cde8f4
thankyou1234 <thankyou7608@naver.com>
2024-11-14 15:37:11 +0900

    2024.11.14 변수

---
+++ program/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace program
+{
+    internal class Program
+    {
+       // Main 함수는 프로그램을 실행하는 시작점을 표시하는 함수입니다.
+        static void Main(string[] args)
+        {
+            #region  변수 
+            // 테잍터를 저장할 수 있는 메모리 공간을 생성하는 것입니다.
+
+            #region 자료형
+            //데이터를 저장하기 위해 데이터의 형태를 정해주는 것입니다
+            // char alphabet = 'A';
+            // int integer = 100;
+            // float pi = 3.141592f;
+            // string name = "Alistar";
+            // 
+            // Console.WriteLine(alphabet);
+            // Console.WriteLine(integer);
+            // Console.WriteLine(pi);
+            // Console.WriteLine(name);
+            // 자료형의 각각의 자료형마다 크기가 정해져 있으며, 자료형의 크기는
+            // 바이트 단위로 이루어져 있고, 자료형의 경우 자료형에 따라 저장할 수
+            // 있는 값의 종류와 범위가 결정되어 있습니다.
+
+
+            #endregion
+
+            #region 변수의 이름 규칙
+
+            // 1. 변수의 이름으로 예약어를 사용할 수 없습니다.
+            // ex) int int;
+
+            // 2.변수의 이름은 숫자로 시작할 수 없습니다.
+            // ex) int 5day;
+
+            // 3. 변수의 이름에 공백이 포함될 수 없습니다.
+            // ex) int level_Up;
+
+            // 4. 변수의 이름으로 특수 기호는 _만 허요이됩니다.
+            // ex) int level_Up;
+
+            // 5. 변수의 이름은 중복이 허용되지 않지만, 대소문자는
+            //    구분할 수 있습니다.
+            // ex) int data = 100;
+            // ex) int DATA = 200;
+            #endregion
+
+            // 변수의 메모리 공간은 프로그램이 실행될 때마다 바뀌며,
+            //여러 개의 변수가 있을 때 서로 고유의 메모리 공간을 가지고 있습니다.
+
+
+
+            #endregion
+
+        }
+    }
+}
