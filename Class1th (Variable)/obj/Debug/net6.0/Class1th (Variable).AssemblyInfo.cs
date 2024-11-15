1b8c150ac19bc1785520e719fd311ed0d062fd68
thankyou1223 <thankyou7608@naver.com>
2024-11-15 15:46:23 +0900

    2024.11.15 비트

--- Class1th (Variable)/obj/Debug/net6.0/Class1th (Variable).AssemblyInfo.cs
+++ Class1th (Variable)/obj/Debug/net6.0/Class1th (Variable).AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("program")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Class1th (Variable)")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23790ade92fdfbed1b4655514ad6fa27b1cde8f4")]
-[assembly: System.Reflection.AssemblyProductAttribute("program")]
-[assembly: System.Reflection.AssemblyTitleAttribute("program")]
+[assembly: System.Reflection.AssemblyProductAttribute("Class1th (Variable)")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Class1th (Variable)")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // MSBuild WriteCodeFragment 클래스에서 생성되었습니다.
